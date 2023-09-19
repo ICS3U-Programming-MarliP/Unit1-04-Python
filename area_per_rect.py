a59dab27643119591eb65f4531acc5a8acc2dac6
Marli Peters <marley.peters@stu.ocsb.ca>
2023-09-19 21:25:28 +0000

    Commited Area Per Rect To Github

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!usr/bin/env/ python3
+# Created By: Marli Peters
+# Date: Sep. 19, 2023
+# This program calculates area and perimeter of a rectangle with dimensions 5cm x 3 cm
+
+
+def main():
+    print("If a rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(5 * 3))
+    print("Perimeter is {}cm".format(2 * (5 + 3)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,19 @@
+#!usr/bin/env/ python3
+# Created By: Marli Peters
+# Date: Sep. 19, 2023
+# This program does basic math.
+
+
+def main():
+    print("8-7={}".format(8 - 7))
+    print("5+9={}".format(5 + 9))
+    print("10*5={}".format(10 * 5))
+    print("7/4={}".format(7 / 4))
+    print("6+3*8={}".format(6 + 3 * 8))
+    print("7^4={}".format(7 ^ 4))  # Bitwise operation
+    print("7**3={}".format(7**3))  # Exponent
+    print("14**0.5={}".format(14**0.5))  # Square root
+
+
+if __name__ == "__main__":
+    main()
